e600d8264957dad441f093ba4e8f3cce345bd940
Sean Lendrum <21slendrum@wpsstudents.org>
2018-03-28 14:14:51 -0400

    Define XboxController, Wheel, and Pedal

--- src/main/java/org.whsrobotics/robot/OI.java
+++ src/main/java/org.whsrobotics/robot/OI.java
@@ -2,6 +2,10 @@ package org.whsrobotics.robot;
 
 public class OI {
 
+    private static XboxController xboxController;
+    private static Wheel wheel;
+    private static Pedal pedal;
+
     public OI() {
 
     }
