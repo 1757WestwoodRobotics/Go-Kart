3923dd64f3fc61f3a2625d1c585c331b7a864ee0
Sean Lendrum <21slendrum@wpsstudents.org>
2018-03-28 14:06:45 -0400

    Initial commit

---
+++ src/main/java/org.whsrobotics/robot/Robot.java
@@ -0,0 +1,101 @@
+package org.whsrobotics.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.command.Scheduler;
+
+public class Robot extends IterativeRobot {
+
+    // Init
+
+    /**
+     * Robot-wide initialization code should go here.
+     * <p>
+     * <p>Users should override this method for default Robot-wide initialization which will be called
+     * when the robot is first powered on. It will be called exactly one time.
+     * <p>
+     * <p>Warning: the Driver Station "Robot Code" light and FMS "Robot Ready" indicators will be off
+     * until RobotInit() exits. Code in RobotInit() that waits for enable will cause the robot to
+     * never indicate that the code is ready, causing the robot to be bypassed in a match.
+     */
+    @Override
+    public void robotInit() {
+
+    }
+
+    /**
+     * Initialization code for disabled mode should go here.
+     * <p>
+     * <p>Users should override this method for initialization code which will be called each time the
+     * robot enters disabled mode.
+     */
+    @Override
+    public void disabledInit() {
+
+    }
+
+
+    // Test Functions
+
+    /**
+     * Initialization code for test mode should go here.
+     * <p>
+     * <p>Users should override this method for initialization code which will be called each time the
+     * robot enters test mode.
+     */
+    @Override
+    public void testInit() {
+
+    }
+
+    /**
+     * Periodic code for test mode should go here.
+     */
+    @Override
+    public void testPeriodic() {
+
+    }
+
+
+    // Autonomous Functions
+
+    /**
+     * Initialization code for autonomous mode should go here.
+     * <p>
+     * <p>Users should override this method for initialization code which will be called each time the
+     * robot enters autonomous mode.
+     */
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    /**
+     * Periodic code for autonomous mode should go here.
+     */
+    @Override
+    public void autonomousPeriodic() {
+        Scheduler.getInstance().run();
+    }
+
+
+    // Periodic Functions
+
+    /**
+     * Initialization code for teleop mode should go here.
+     * <p>
+     * <p>Users should override this method for initialization code which will be called each time the
+     * robot enters teleop mode.
+     */
+    @Override
+    public void teleopInit() {
+
+    }
+
+    /**
+     * Periodic code for teleop mode should go here.
+     */
+    @Override
+    public void teleopPeriodic() {
+        Scheduler.getInstance().run();
+    }
+}
