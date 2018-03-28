6ae64acab1bc5345168f8819a01186a1d9b8d31f
Sean Lendrum <21slendrum@wpsstudents.org>
2018-03-28 14:50:12 -0400

    Reduce Errors

--- src/main/java/org.whsrobotics/robot/OI.java
+++ src/main/java/org.whsrobotics/robot/OI.java
@@ -2,10 +2,6 @@ package org.whsrobotics.robot;
 
 public class OI {
 
-    private static XboxController xboxController;
-    private static Wheel wheel;
-    private static Pedal pedal;
-
     public OI() {
 
     }
