a56462ddb4e9b838a3b9336cdc69ede4cbaa3db0
Sean Lendrum <21slendrum@wpsstudents.org>
2018-03-31 18:06:52 -0400

    Define and Declare FlightStick, Implement FlightStickDrive, Add an arcade drive mode for singular flight stick

---
+++ src/main/java/org.whsrobotics/commands/FlightStickDrive.java
@@ -0,0 +1,30 @@
+package org.whsrobotics.commands;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.command.Command;
+import org.whsrobotics.robot.OI;
+import org.whsrobotics.subsystems.DriveTrain;
+
+public class FlightStickDrive extends Command {
+
+    public FlightStickDrive() {
+        requires(DriveTrain.getInstance());
+    }
+
+    @Override
+    protected void execute() {
+
+        DriveTrain.flightStickDrive(OI.getFlightStick().getX(GenericHID.Hand.kRight),
+                OI.getFlightStick().getY(GenericHID.Hand.kRight));
+    }
+
+    @Override
+    protected void end() {
+        DriveTrain.stopDrive();
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
