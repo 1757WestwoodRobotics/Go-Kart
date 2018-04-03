fe5f7d35df500afe07d640539b6942af8be15692
Sean Lendrum <21slendrum@wpsstudents.org>
2018-04-02 20:37:41 -0400

    Correct rotation, set default command, set flight stick to port 0, get instance of Drivetrain and OI

--- src/main/java/org.whsrobotics/robot/OI.java
+++ src/main/java/org.whsrobotics/robot/OI.java
@@ -1,12 +1,14 @@
 package org.whsrobotics.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import org.whsrobotics.subsystems.DriveTrain;
 
 import static org.whsrobotics.robot.RobotMap.FLIGHTSTICK;
 
 public class OI {
 
     private static Joystick flightStick;
+    private static OI instance;
 
 
     private OI() {
@@ -23,4 +25,12 @@ public class OI {
         return value;
     }
 
+
+    public static OI getInstance() {
+        if (instance == null) {
+            instance = new OI();
+        }
+
+        return instance;
+    }
 }
--- src/main/java/org.whsrobotics/robot/RobotMap.java
+++ src/main/java/org.whsrobotics/robot/RobotMap.java
@@ -6,6 +6,7 @@ public class RobotMap {
 
     public enum MotorControllers {
 
+        //TODO: Set to the right talons
         DRIVE_LEFT_FRONT(1), DRIVE_RIGHT_FRONT(2),
         DRIVE_LEFT_BACK(3), DRIVE_RIGHT_BACK(4),
         DRIVE_LEFT_MIDDLE(5), DRIVE_RIGHT_MIDDLE(6);
@@ -18,7 +19,7 @@ public class RobotMap {
 
     }
 
-    public static final int FLIGHTSTICK = 3;
+    public static final int FLIGHTSTICK = 0;
 
     public static void init() {
 
